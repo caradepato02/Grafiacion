ce2709b5fd56264d16e17bb637225c6ba63f991a
Kepin <kepin.ortiz@gmail.com>
2020-10-19 22:14:23 -0600

    Fondo detallado y enemigo agregado

---
+++ Test/Ants 2D/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject target;
+    private Vector3 targetPosition;
+    public float cameraSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPosition = new Vector3(target.transform.position.x,
+                                    target.transform.position.y,
+                                    this.transform.position.z);
+    }
+
+     //public Transform followTransform;
+    
+
+    // Update is called once per frame
+    public void LateUpdate()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, Time.deltaTime * cameraSpeed);
+        //this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
+        
+        
+    }
+}
+/*
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPosition = new Vector3(target.transform.position.x,
+                                    target.transform.position.y,
+                                    this.transform.position.z);
+
+    }
+    private void LateUpdate()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, targetPosition ,Time.deltaTime * cameraSpeed);
+    }
+}*/
---
+++ Test/Ants 2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private const string AXIS_H = "Horizontal", AXIS_V ="Vertical";
+    
+    public float velocidadRotacion = 100;
+    public float velocidadMovimiento = 5;
+    private bool camina = false;
+    private bool ataca = false;
+    private Animator _animator;
+    private Rigidbody2D _rigidbody;
+
+    // Start is called before the first frame update
+    void Start () {
+         _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+         camina = false;
+         ataca= false;
+        if(Mathf.Abs(Input.GetAxisRaw(AXIS_H))>0.2f){
+           
+            transform.Rotate(Vector3.forward * -Input.GetAxisRaw(AXIS_H) * velocidadRotacion * Time.deltaTime);
+            camina = true;
+        }
+        if(Input.GetAxisRaw(AXIS_V)>0.2f){
+            transform.position += (transform.up  * velocidadMovimiento *  Time.deltaTime);
+            camina = true;
+        }
+        if(Input.GetAxisRaw(AXIS_V)< -0.2f){
+            transform.position += (-transform.up * velocidadMovimiento/2 *  Time.deltaTime);
+            camina = true;
+        }
+
+        if(Input.GetMouseButtonDown (0)){
+            ataca = true;
+        }
+
+         if(!camina){
+            _rigidbody.velocity = Vector2.zero;
+        }
+      
+    }
+
+    private void LateUpdate() {
+        _animator.SetFloat(AXIS_H,Input.GetAxisRaw(AXIS_H));
+        _animator.SetFloat(AXIS_V,Input.GetAxisRaw(AXIS_V));
+        _animator.SetBool("camina",camina);
+        _animator.SetBool("ataca",ataca);
+    }
+}
